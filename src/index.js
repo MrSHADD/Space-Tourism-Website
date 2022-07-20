2c405de337be3305ed07001c8186fb4331741264
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-07-20 21:11:01 +0100

    Create index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import Destination from "./Destination"
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Destination from "./components/Destination";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import FrontPage from "./components/FrontPage"
 
-ReactDOM.render(<App/>,  document.getElementById("root"))
+export default function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FrontPage />}>
+            <Route index element={<Destination />} />
+            <Route path="destination" element={<Destination />} />
+            {/* <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
